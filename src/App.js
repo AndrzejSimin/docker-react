97a0bd57fc4f0e3017113a05ac9c84efedc387e2
Andrzej Simiński <andrzej.simin@gmail.com>
2023-01-24 19:24:40 +0100

    simin branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hehehexxxxxsss! <code>src/App.js</code> and save to reload.
+          SIMIN FIXED AWS DEPLOY! <code>src/App.js</code> FINALLY!
         </p>
         <a
           className="App-link"
